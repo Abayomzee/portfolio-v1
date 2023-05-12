bd6b5e61909d6fbb5d19630f2f19ce086eca5075
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-05-12 02:30:07 +0100

    fix:

--- src/Components/Atom/SvgIcons/index.tsx
+++ src/Components/Atom/SvgIcons/index.tsx
@@ -2,5 +2,5 @@ export { default as ArrowIcon } from "./ArrowIcon";
 export { default as EmailIcon } from "./EmailIcon";
 export { default as InstagramIcon } from "./InstagramIcon";
 export { default as TwitterIcon } from "./TwitterIcon";
-export { default as GithubIcon } from "./GithubIcon";
 export { default as LinkedInIcon } from "./LinkedInIcon";
+export { default as GithubIcon } from "./GithubIcon";
