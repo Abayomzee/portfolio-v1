134642a65e6a90e94eb962808f978d9cf28d82c5
Okegbenro Abayomi <okegbenroabayo@gmail.com>
2023-05-20 06:39:46 +0100

    fix:

--- src/Components/Molecules/ProjectCard/index.tsx
+++ src/Components/Molecules/ProjectCard/index.tsx
@@ -9,7 +9,7 @@ const ProjectCard: React.FC<ProjectCardProps> = (props) => {
   // Props
   const { col } = props;
 
-  // Data to renderr
+  // Data to renderrr
   return (
     <a
       href={col.url ? col.url : "#"}
